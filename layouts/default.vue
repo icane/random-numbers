411cc58e5a2850eb43bb92f5bb517afea285190e
Alejandro López Cagigas <externo.lopez_ale@cantabria.es>
2021-03-02 15:55:28 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app color="primary">
+      <v-toolbar-title class="heading-primary" v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer padless>
+      <v-card width="100%" flat tile class="text-center">
+        <v-card-text class="primary">
+          <v-row no-gutters>
+            <v-row align="center" justify="center">
+              <a href="https://www.icane.es" target="blank">
+                <v-img
+                  alt="Logo ICANE"
+                  src="logo/logo_icane.svg"
+                  transition="scale-transition"
+                  max-width="150"
+                />
+              </a>
+            </v-row>
+            <v-row align="center" justify="center">
+              <a href="https://www.cantabria.es" target="blank">
+                <v-img
+                  alt="Logo Cantabria"
+                  src="consejeria_hacienda/blanco_negro.svg"
+                  transition="scale-transition"
+                  max-width="150"
+                />
+              </a>
+            </v-row>
+            <v-row align="center" justify="center">
+              <a href="https://www.caminolebaniego.com" target="blank">
+                <v-img
+                  alt="Logo Camino Lebaniego"
+                  src="logo/camino_lebaniego_blanco.svg"
+                  transition="scale-transition"
+                  max-width="150"
+                />
+              </a>
+            </v-row>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Sorteo de direcciones',
+    }
+  },
+}
+</script>
+<style scoped>
+.heading-primary {
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6" class="ma-3 pa-3">
+      <v-card>
+        <v-card-text>
+          <v-form ref="form" lazy-validation @submit="generate">
+            <v-row>
+              <v-col class="pr-4">
+                <v-text-field
+                  v-model="dir_num"
+                  type="number"
+                  label="Número total de direcciones"
+                  :rules="[numberRule]"
+                ></v-text-field>
+                <v-text-field
+                  v-model="select_dir"
+                  type="number"
+                  label="Número de direcciones a seleccionar"
+                  :rules="[numberRule]"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+              <v-btn class="ma-3 pa-3" color="primary" @click="generate"
+                >Generar</v-btn
+              >
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import papaparse from 'papaparse'
+
+export default {
+  components: {},
+  data() {
+    return {
+      dir_num: 10,
+      dir_num_min: 0,
+      select_dir: 5,
+      selected_dirs: [],
+      numberRule: (v) => {
+        if (!isNaN(parseInt(v)) && v >= 1) return true
+        return 'El número debe ser mayor que 1'
+      },
+    }
+  },
+  methods: {
+    generate() {
+      this.dir_num = parseInt(this.dir_num)
+      this.select_dir = parseInt(this.select_dir)
+      this.selected_dirs = []
+      const array = [...Array(this.dir_num + 1).keys()]
+      array.shift() // array starts at 1
+      console.log(array)
+
+      for (let i = 0; i < this.select_dir; i++) {
+        const random = Math.floor(Math.random() * array.length)
+        this.selected_dirs.push({ number: array[random] })
+        array.splice(random, 1)
+      }
+      console.log(this.selected_dirs)
+      const csv = papaparse.unparse(this.selected_dirs, {
+        delimiter: ';',
+        encoding: 'ISO-8859-1',
+        skipEmptyLines: true,
+      })
+      console.log(csv)
+      this.download('Direcciones_' + new Date() + '.csv', csv)
+    },
+    getRandomInt(min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    download(filename, text) {
+      if (window.navigator.msSaveBlob) {
+        // IE y Edge
+        const blob = new Blob([new Uint8Array([0xef, 0xbb, 0xbf]), text], {
+          type: 'text/plain;charset=utf-8;',
+        })
+        window.navigator.msSaveBlob(blob, filename)
+      } else {
+        const element = document.createElement('a')
+        element.setAttribute(
+          'href',
+          'data:text/plain;charset=utf-8,%EF%BB%BF' + encodeURIComponent(text)
+        )
+        element.setAttribute('download', filename)
+        element.style.display = 'none'
+        document.body.appendChild(element)
+        element.click()
+        document.body.removeChild(element)
+      }
+    },
+  },
+}
+</script>
